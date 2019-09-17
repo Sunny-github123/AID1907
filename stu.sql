e0f1849e6c0af891b2d6b50e794d40808dc78b33
Tedu <737692077@qq.com>
2019-09-17 10:44:22 +0800

    add dict

---
+++ stu.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: stu
+-- ------------------------------------------------------
+-- Server version	5.7.25-0ubuntu0.18.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `class_1`
+--
+
+DROP TABLE IF EXISTS `class_1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `class_1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `age` tinyint(3) unsigned NOT NULL,
+  `gender` enum('m','w','o') DEFAULT NULL,
+  `tel` char(16) DEFAULT NULL,
+  `score` float DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `class_1`
+--
+
+LOCK TABLES `class_1` WRITE;
+/*!40000 ALTER TABLE `class_1` DISABLE KEYS */;
+INSERT INTO `class_1` VALUES (1,'Alex',17,'m',NULL,68.5),(2,'Abby',18,'w',NULL,95),(3,'Tom',18,'m',NULL,77),(5,'Lily',19,'w',NULL,60);
+/*!40000 ALTER TABLE `class_1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `interest`
+--
+
+DROP TABLE IF EXISTS `interest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `interest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `hobby` set('sing','dance','draw','basketball') DEFAULT NULL,
+  `level` char(1) NOT NULL,
+  `price` decimal(6,2) DEFAULT NULL,
+  `comment` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `interest`
+--
+
+LOCK TABLES `interest` WRITE;
+/*!40000 ALTER TABLE `interest` DISABLE KEYS */;
+INSERT INTO `interest` VALUES (1,'Joy','sing,dance','B',1388.00,'舞林奇才');
+/*!40000 ALTER TABLE `interest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marathon`
+--
+
+DROP TABLE IF EXISTS `marathon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marathon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `athlete` varchar(32) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `registration_time` datetime DEFAULT NULL,
+  `performance` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marathon`
+--
+
+LOCK TABLES `marathon` WRITE;
+/*!40000 ALTER TABLE `marathon` DISABLE KEYS */;
+INSERT INTO `marathon` VALUES (1,'曹操','1992-05-02','2019-06-08 14:23:50','02:36:48'),(2,'刘备','1992-08-09','2019-06-08 15:43:26','03:26:58'),(3,'关羽','1989-07-06','2019-07-01 18:21:36','02:16:45');
+/*!40000 ALTER TABLE `marathon` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-16 11:52:37
